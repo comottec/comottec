6f1382674e6fc6af758b8c99edca9829d6d1c0ad
Cristiano Ferreira <cristiano@comottec.com.br>
2025-05-21 11:01:41 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+﻿<?xml version="1.0" encoding="utf-8"?><Error><Code>OutOfRangeInput</Code><Message>One of the request inputs is out of range.
+RequestId:a8880d23-101e-0004-0857-cad6ad000000
+Time:2025-05-21T13:51:41.8971262Z</Message></Error>
