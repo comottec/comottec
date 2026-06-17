bbb5aa7236fc0ab1344f2f4eda68d18cafad2af4
Cristiano Ferreira <cristiano@comottec.com.br>
2026-06-17 20:06:37 -0300

    fix poath

--- scripts/ofi/qa_2510/install.ps1
+++ scripts/ofi/qa_2510/install.ps1
@@ -2,7 +2,7 @@
 
 $APPSERVER_ENDPOINT = "https://protheus.ofi.com:6110"
 $INSTALATION_ROOT = "c:\Totvs_WebAgent"
-$INSTALATION_DIR = (join-path -Path $INSTALATION_ROOT -ChildPath "Totvs2510_qa")
+$INSTALATION_DIR = (join-path -Path $INSTALATION_ROOT -ChildPath "Totvs2510_OGA_QA")
 $WEBAGENT_URL = "https://comottecstore.blob.core.windows.net/`$web/files/ofi/totvs2510_qa.zip"
 $TEMP_DIR = [System.IO.Path]::GetTempPath()
 $TEMP_FILE = (Join-Path -Path $TEMP_DIR -ChildPath "Totvs2510_qa.zip")
