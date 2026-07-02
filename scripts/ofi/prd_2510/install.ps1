ac1c10f981d24c25fd19246a02bec28a9db8b530
Cristiano Ferreira <cristiano@comottec.com.br>
2026-07-02 10:41:15 -0300

    prd

---
+++ scripts/ofi/prd_2510/install.ps1
@@ -0,0 +1,72 @@
+## powershell.exe -executionpolicy bypass -file "install.ps1"
+
+$APPSERVER_ENDPOINT = "https://protheus.ofi.com:5010"
+$INSTALATION_ROOT = "c:\Totvs_WebAgent"
+$INSTALATION_DIR = (join-path -Path $INSTALATION_ROOT -ChildPath "Totvs2510_Prd")
+$WEBAGENT_URL = "https://comottecstore.blob.core.windows.net/`$web/files/ofi/Totvs2510_prd.zip"
+$TEMP_DIR = [System.IO.Path]::GetTempPath()
+$TEMP_FILE = (Join-Path -Path $TEMP_DIR -ChildPath "Totvs2510_prd.zip")
+
+function get-browser {
+    $process = Start-Process -FilePath 'http://www.google.com' -PassThru -WindowStyle Hidden
+    $browserApp = $process.Path
+    $process.Kill()
+    return $browserApp
+}
+
+function get-SmartApp {
+    echo "Baixando arquivos para instalação..."
+
+    if (test-path $TEMP_FILE) {
+        Remove-Item -Path $TEMP_FILE  -Force
+    }
+
+    Invoke-WebRequest -OutFile $TEMP_FILE -Uri $WEBAGENT_URL
+
+    if (-not (test-path $INSTALATION_ROOT)) {
+        md $INSTALATION_ROOT
+    }
+
+    if (test-path $INSTALATION_DIR) {
+        Remove-Item -Path $INSTALATION_DIR -Recurse -Force
+    }
+    Expand-Archive -Path $TEMP_FILE -DestinationPath $INSTALATION_ROOT 
+}
+
+function create-Shortcut {
+    echo "Criando atalhos..."
+    $localBrowser = (get-browser)
+    $WshShell = New-Object -ComObject WScript.Shell
+    $ShortcutFile = (join-path -Path $INSTALATION_DIR -ChildPath "Totvs2510_prd.lnk")
+
+    if (test-path $ShortcutFile) {
+        Remove-Item -Path $ShortcutFile -Force
+    }
+
+    $Shortcut = $WshShell.CreateShortcut($ShortcutFile)
+    $Shortcut.TargetPath = (join-path -Path $INSTALATION_DIR -ChildPath "web-agent.exe")
+    $Shortcut.Arguments = "launch `"$APPSERVER_ENDPOINT/webapp/?p=SIGAMDI`&e=producao`&M=1`" --browser=`"$localBrowser`" "
+    # $IconLocation = "C:\Windows\System32\shell32.dll,290"
+    $IconLocation = "C:\Windows\System32\shell32.dll,164"
+    $Shortcut.IconLocation = $IconLocation
+    $Shortcut.Save()
+    
+    $desktopPath = [Environment]::GetFolderPath("Desktop")
+    copy-item -Path $ShortcutFile -Destination $desktopPath 
+}
+
+# function import-certTotvs {
+#     echo "Importando certificados Totvs..."
+#     Import-Certificate -FilePath (join-path -Path $INSTALATION_DIR -ChildPath "totvs_certificate_CA.crt") -CertStoreLocation  'Cert:\LocalMachine\Root' 
+# }
+
+function instalation-program {
+    get-SmartApp
+    create-Shortcut
+    # import-certTotvs
+}
+
+echo "Iniciando programa de instalação..."
+instalation-program
+echo "Instalação concluída."
+
